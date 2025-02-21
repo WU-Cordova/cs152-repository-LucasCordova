4561b1ce47638df90c714cab950e9cf153f4f77d
LucasCordova <lpcordova@outlook.com>
2025-02-21 12:00:59 -0800

    class

--- program.py
+++ program.py
@@ -1,16 +1,27 @@
 
 
+from datastructures.array2d import Array2D
 from datastructures.bag import Bag
+from tests.car import Car
 
 
 def main():
     
-    print("Hello, World!")
+    data_type = Car
 
-    bag = Bag()
-    bag.add('apple')
+    rows_len, cols_len = 3, 2
 
-    print(bag.count('apple'))
+    sequence = [[data_type() for _ in range(cols_len)] for _ in range(rows_len)]
+
+    sequence2 = []
+    for row_index in range(rows_len):
+        sequence.append([])
+
+    for row_index in range(rows_len):
+        for col_index in range(cols_len):
+            sequence2[row_index].append(data_type())
+
+    return Array2D(starting_sequence=sequence, data_type=data_type)
 
     
 
